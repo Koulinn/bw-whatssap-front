9d1234ff2d6c43fa5025e5df39aadedd406df764
Koulinn <drdverzola@gmail.com>
2021-10-11 18:42:42 +0100

    search input done

--- src/views/AppPage/components/SmallColumn/User/SearchBar.jsx
+++ src/views/AppPage/components/SmallColumn/User/SearchBar.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import InputDefault from '../../../../../components/Login_Register_Shareable/InputDefault/InputDefault'
 import { useState } from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 export const SearchBar = (props) => {
     const [searchValue, setSearchValue] = useState('')
     return (
-        <div className="form_input d-flex flex-column" >
-            <label className="invisible" htmlFor='searchBar'>Search Bar</label>
-            <input className="w-100" id='searchBar' type="text" placeholder='Start a new chat or search your friends' onChange={(e) => setSearchValue(e.target.value)} />
+        <div id="searchBar-wrapper" className="form_input d-flex align-items-center bg-grey p-3" >
+            <div className="bg-white w-100 d-flex align-items-center">
+                <label htmlFor='searchBar' className="ml-2">
+                    <AiOutlineSearch  />
+                </label>
+            <input className="w-100" id="searchBar" type="text" placeholder='Search or start a new chat' onChange={(e) => setSearchValue(e.target.value)} />
+            </div>
         </div>
     )
 }
