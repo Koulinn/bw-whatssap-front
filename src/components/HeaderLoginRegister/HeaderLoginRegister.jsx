2e52e2e8e0d28a9ac30ffc13092112c5ec6743f1
Koulinn <drdverzola@gmail.com>
2021-10-11 11:32:11 +0100

    Login

--- src/components/HeaderLoginRegister/HeaderLoginRegister.jsx
+++ src/components/HeaderLoginRegister/HeaderLoginRegister.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Header_Login_Register() {
+function HeaderLoginRegister() {
     return (
         <div className="d-flex px-3 pt-4 pb-64 bg-green">
             <img src="https://static.whatsapp.net/rsrc.php/ym/r/36B424nhiL4.svg" alt="" />
@@ -9,4 +9,4 @@ function Header_Login_Register() {
     )
 }
 
-export default Header_Login_Register
+export default HeaderLoginRegister
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
+import { Container, Row } from 'react-bootstrap'
 import { connect } from 'react-redux'
-import Header_Login_Register from '../../components/Header_login_register/Header_Login_Register'
+import HeaderLoginRegister from '../../components/HeaderLoginRegister/HeaderLoginRegister'
+import { useState, useEffect } from 'react'
+import InputDefault from '../../components/InputDefault/InputDefault'
 
 export const Login = (props) => {
+    const [userEmail, setUserEmail] = useState('')
+    const [userPassword, setUserPassword] = useState('')
+    
     return (
-        <div>
-            <Header_Login_Register/>
+        <div id="login">
+            <HeaderLoginRegister />
+            <Container className="bg-grey px-5" fluid>
+                <Container>
+                    <Row className="p-5  bg-white">
+                        <div className="col-8">
+                            <h2 className="mb-2">Login</h2>
+                            <InputDefault inputID="email" setFunc={setUserEmail} placeholderText='Enter your email' label="E-mail"/>
+                            <InputDefault inputID="password" setFunc={setUserPassword} placeholderText='Enter your password' label="Password"/>
+    
+                        </div>
+                        <div className="col-4 d-flex justify-content-center align-items-center">
+                            <img src="https://static.whatsapp.net/rsrc.php/yz/r/lOol7j-zq4u.svg" width="128" alt="" />
+                        </div>
+                       
+    
+                    </Row>
+                </Container>
+            </Container>
         </div>
     )
 }
 
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
