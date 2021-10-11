cbbe111edbe60a248ff45b505d20a794891a1e47
Koulinn <drdverzola@gmail.com>
2021-10-11 14:25:31 +0100

    Login finished

--- src/components/InputDefault/InputDefault.jsx
+++ src/components/InputDefault/InputDefault.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function InputDefault({inputID, setFunc, placeholderText, label}) {
     return (
         <div className="form_input d-flex flex-column mt-4" >
-            <label className="mr-3 mb-3 col-2 pl-0 d-flex align-items-center" htmlFor={inputID}>{label}</label>
+            <label className="mr-3 mb-3 pl-0 d-flex align-items-center" htmlFor={inputID}>{label}</label>
             <input className="col-8" id={inputID} type="text" placeholder={placeholderText} onChange={(e) => setFunc(e.target.value)} />
         </div>
     )
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Button, Container, Row } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import HeaderLoginRegister from '../../components/HeaderLoginRegister/HeaderLoginRegister'
 import { useState, useEffect } from 'react'
 import InputDefault from '../../components/InputDefault/InputDefault'
+import GoogleButton from 'react-google-button'
 
 export const Login = (props) => {
     const [userEmail, setUserEmail] = useState('')
     const [userPassword, setUserPassword] = useState('')
-    
+
     return (
         <div id="login">
             <HeaderLoginRegister />
@@ -17,15 +18,32 @@ export const Login = (props) => {
                     <Row className="p-5  bg-white register_login_border_shadow_translate">
                         <div className="col-8">
                             <h2 className="mb-5">Login</h2>
-                            <InputDefault inputID="email" setFunc={setUserEmail} placeholderText='Enter your email' label="E-mail"/>
-                            <InputDefault inputID="password" setFunc={setUserPassword} placeholderText='Enter your password' label="Password"/>
-    
+                            <InputDefault inputID="email" setFunc={setUserEmail} placeholderText='Enter your email' label="E-mail" />
+                            <InputDefault inputID="password" setFunc={setUserPassword} placeholderText='Enter your password' label="Password" />
                         </div>
                         <div className="col-4 d-flex justify-content-center align-items-center">
                             <img src="https://static.whatsapp.net/rsrc.php/yz/r/lOol7j-zq4u.svg" width="128" alt="" />
                         </div>
-                       
-    
+
+
+                        <div className="col-12 d-flex pl-3 pt-5 mt-5 justify-content-center">
+                            <Button variant="success">Login</Button>
+                        </div>
+
+                        <div className="col-12 d-flex align-items-center my-5">
+                            <hr className="mr-0" />
+                            <span className="px-3 d-flex align-items-center translate_-2px">or</span>
+                            <hr className="ml-0" />
+                        </div>
+                        <div className="col-12 d-flex justify-content-center">
+                            <GoogleButton className="googleBtn" />
+                        </div>
+
+                        <div className="col-12 d-flex justify-content-center mt-5">
+                            <p>First time? <strong>Create an account</strong> </p>
+
+                        </div>
+
                     </Row>
                 </Container>
             </Container>
