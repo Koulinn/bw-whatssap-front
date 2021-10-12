55bf6f8d2b2a4670d767f6ea58e13c2aebe50970
Koulinn <drdverzola@gmail.com>
2021-10-12 12:06:14 +0100

    Created UserCard

--- src/views/AppPage/components/SmallColumn/CreateRoom/CreateRoom.jsx
+++ src/views/AppPage/components/SmallColumn/CreateRoom/CreateRoom.jsx
@@ -1,30 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {MdArrowBack} from 'react-icons/md'
+import { MdArrowBack } from 'react-icons/md'
 import ReturnMain from '../ShareableComp/ReturnMain'
 import { SearchContacs } from '../ShareableComp/SearchContacs'
+import { ChatRoom } from '../RecentChatColumn/Components/ChatRoom/ChatRoom'
+import UserCard from './UserCard/UserCard'
 
 export const CreateRoom = (props) => {
     return (
-        <div id="createRoom" className="d-flex flex-column h-100 bg-green ">
-           <header className="d-flex" style={{height:'149px'}}>
-               <ReturnMain icon={<MdArrowBack/>} title="Add group participants"/>
+        <div id="createRoom" className="d-flex flex-column h-100 bg-green-light ">
+            <header className="d-flex" style={{ height: '149px' }}>
+                <ReturnMain icon={<MdArrowBack />} title="Add group participants" />
             </header>
-           <div className="h-100 d-flex flex-column bg-white">
-               <SearchContacs/>
+            <div className="d-flex flex-column bg-white" style={{ height: '85.55%' }}>
+                <SearchContacs />
+                <div className="contacts-wrapper">
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
+                    <UserCard name="randomName" bio="I love cake"/>
 
-           </div>
-            
+                    
+    
+                </div>
+
+            </div>
+
         </div>
     )
 }
 
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateRoom)
