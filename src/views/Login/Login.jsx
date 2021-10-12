4efe7f8ab3cc7068513e2b0d87f09f30b30fcb48
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-12 13:01:08 +0200

    login fetch

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -6,11 +6,36 @@ import { useState, useEffect } from 'react'
 import InputDefault from '../../components/Login_Register_Shareable/InputDefault/InputDefault'
 import GoogleButton from 'react-google-button'
 import Divisor from '../../components/Login_Register_Shareable/Divisor/Divisor'
-
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export const Login = (props) => {
-    const [userEmail, setUserEmail] = useState('')
-    const [userPassword, setUserPassword] = useState('')
+    const [login, setLogin] = useState({email:'',password:''})
+    const [redirect, setreDirect] = useState(false)
+
+    const handleInput = (key,value)=>{
+        setLogin(
+           { ...login,
+          [key]:value}
+          )
+}
+    const userLogin = async () => {
+        try {
+            const serverResponse = await axios.post(`http://localhost:3001/user/login`, { email:login.email,password:login.password })
+            if(serverResponse.status === 200){
+                console.log(serverResponse.data)
+                setreDirect(true)
+            } else {
+                console.log('else')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    
+    }
+    if(redirect){
+        return <Redirect to='/'/>
+    }
 
     return (
         <div id="login">
@@ -20,10 +45,10 @@ export const Login = (props) => {
                     <Row className="p-5  bg-white register_login_border_shadow_translate">
                         <div className="col-8">
                             <h2 className="mb-5">Login</h2>
-                            <InputDefault inputID="email" setFunc={setUserEmail} placeholderText='Enter your email' label="E-mail" />
-                            <InputDefault inputID="password" setFunc={setUserPassword} placeholderText='Enter your password' label="Password" />
+                            <InputDefault inputID='email' login={login} value={login.email}  handleInput={handleInput} placeholderText='Enter your email' label="E-mail" />
+                            <InputDefault inputID='password' login={login} value={login.password}   handleInput={handleInput}  placeholderText='Enter your password' label="Password" />
                             <div className="col-8 d-flex pl-3 pt-5 mt-5 justify-content-center">
-                                <Button variant="success">Login</Button>
+                                <Button variant="success" onClick={userLogin}>Login</Button>
                             </div>
 
                             <Divisor/>
