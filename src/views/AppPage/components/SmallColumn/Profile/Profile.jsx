4bcc4d31d0fea9f119681d592733ae82dbcb9cca
Koulinn <drdverzola@gmail.com>
2021-10-13 10:50:13 +0100

    profile connected

--- src/views/AppPage/components/SmallColumn/Profile/Profile.jsx
+++ src/views/AppPage/components/SmallColumn/Profile/Profile.jsx
@@ -3,10 +3,16 @@ import { connect } from 'react-redux'
 import ReturnMain from '../ShareableComp/ReturnMain'
 import { MdArrowBack, MdOutlineEdit, MdOutlineCheck } from 'react-icons/md'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
 
-export const Profile = (props) => {
+export const Profile = () => {
     const [usernameEditMode, setUsernameEditMode] = useState(false)
     const [aboutEditMode, setAboutEditMode] = useState(false)
+    const userData = useSelector(state=>state.user.userData)
+    const{avatar, name, email, bio} = userData
+
+    
+    console.log(userData, 'props from profiileeee')
     return (
         <div id="userProfile" className="d-flex flex-column h-100 bg-green-light ">
             <header className="d-flex" style={{ height: '149px' }}>
@@ -16,16 +22,16 @@ export const Profile = (props) => {
                 <div className="my-4">
                     <div className="d-flex justify-content-center position-relative">
                         <label htmlFor="userAvatar">
-                            <img width="200" height="200" style={{ borderRadius: "500px" }} src="https://www.seekpng.com/png/full/115-1150456_avatar-generic-avatar.png" alt="" />
+                            <img width="200" height="200" style={{ borderRadius: "500px" }} src={avatar} alt="" />
                         </label>
                         <input id="userAvatar" type="file" className="invisible position-absolute" />
                     </div>
                 </div>
                 <div className="my-4 bg-light-grey py-3 shadow-effect">
-                    <label htmlFor="userNameInput" className="mx-3 pl-3">Your name</label>
+                    <label htmlFor="userNameInput" className="mx-3 pl-3">{name}</label>
                     <div className="d-flex justify-content-between align-items-center">
 
-                        <input type="text" id="userNameInput" className="bg-transparent mx-3" placeholder="User name" onFocus={() => setUsernameEditMode(true)} onBlur={() => setUsernameEditMode(false)} />
+                        <input type="text" id="userNameInput" className="bg-transparent mx-3" placeholder={name} onFocus={() => setUsernameEditMode(true)} onBlur={() => setUsernameEditMode(false)} />
                         <div className="d-flex justify-content-center align-items-center mr-4 icon-wrapper">
                             {usernameEditMode ? <MdOutlineCheck /> : <MdOutlineEdit />}
                         </div>
@@ -38,7 +44,7 @@ export const Profile = (props) => {
                     <label htmlFor="aboutInput" className="mx-3 pl-3">About</label>
                     <div className="d-flex justify-content-between align-items-center">
 
-                        <input type="text" id="aboutInput" className="bg-transparent mx-3" placeholder="Available" onFocus={() => setAboutEditMode(true)} onBlur={() => setAboutEditMode(false)} />
+                        <input type="text" id="aboutInput" className="bg-transparent mx-3" placeholder={bio? bio : 'Please walk with me!'} onFocus={() => setAboutEditMode(true)} onBlur={() => setAboutEditMode(false)} />
                         <div className="d-flex justify-content-center align-items-center mr-4 icon-wrapper" >
                             {aboutEditMode ? <MdOutlineCheck /> : <MdOutlineEdit />}
                         </div>
@@ -50,12 +56,10 @@ export const Profile = (props) => {
     )
 }
 
-const mapStateToProps = (state) => ({
 
-})
 
 const mapDispatchToProps = {
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile)
+export default connect(mapDispatchToProps)(Profile)
