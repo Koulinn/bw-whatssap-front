531e40b3625d7d3b533771f4c093e1b7bf6cd5c9
Koulinn <drdverzola@gmail.com>
2021-10-15 11:35:15 +0100

    added persist storage

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -26,22 +26,22 @@ export const groupedReducers = combineReducers({
 })
 
 
-// const configPersistance = {
-//     key: 'root',
-//     storage,
-//     transforms: [
-//         encryptTransform({
-//             secretKey: process.env.REACT_APP_REDUX_STORAGE_SECRET_KEY
-//         }),
-//     ]
-// }
+const configPersistance = {
+    key: 'root',
+    storage,
+    transforms: [
+        encryptTransform({
+            secretKey: process.env.REACT_APP_REDUX_STORAGE_SECRET_KEY
+        }),
+    ]
+}
 
-// export const persistedReducer = persistReducer(configPersistance, groupedReducers)
+export const persistedReducer = persistReducer(configPersistance, groupedReducers)
 
 const useThunk = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(thunk)) : applyMiddleware(thunk)
 
 const configureStore = createStore(
-    groupedReducers,
+    persistedReducer,
     initialState,
     useThunk
 )
